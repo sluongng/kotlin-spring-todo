95a7d598862c359926707575d1571894fd758c1f
Son Luong Ngoc <sluongng@gmail.com>
2019-07-08 13:47:44 +0700

    Added Log4j2, fixed test running from maven cli

--- src/main/kotlin/com/sluongng/demo/adapter/web/HelloController.kt
+++ src/main/kotlin/com/sluongng/demo/adapter/web/HelloController.kt
@@ -1,5 +1,6 @@
 package com.sluongng.demo.adapter.web
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -8,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/hello")
 class HelloController {
 
+    val logger = LoggerFactory.getLogger(this.javaClass)!!
+
     @GetMapping
     fun hello(): String {
+        logger.info("hello() invoked")
+
         return "hello world"
     }
 }
--- src/test/kotlin/com/sluongng/demo/adapter/web/HelloControllerTest.kt
+++ src/test/kotlin/com/sluongng/demo/adapter/web/HelloControllerTest.kt
@@ -2,14 +2,20 @@ package com.sluongng.demo.adapter.web
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.platform.commons.logging.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.http.HttpStatus
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HelloControllerTest(@Autowired val restTemplate: TestRestTemplate) {
+
+    val logger = LoggerFactory.getLogger(this.javaClass)!!
 
     @Test
     fun `hello api is working`() {
@@ -18,5 +24,7 @@ class HelloControllerTest(@Autowired val restTemplate: TestRestTemplate) {
         assertThat(response).isNotNull
         assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
         assertThat(response.body).isEqualTo("hello world")
+
+        logger.info { "Test succeed" }
     }
 }
