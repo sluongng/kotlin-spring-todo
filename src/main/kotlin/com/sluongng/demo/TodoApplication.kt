5cf1e7c61e16b71f30ef4c5115ad317fbdcb9a6b
Son Luong Ngoc <sluongng@gmail.com>
2019-07-23 10:15:34 +0700

    Fixed Method is not under package directly, Docs: https://discuss.kotlinlang.org/t/is-the-main-function-must-be-a-standalone-function/877

--- src/main/kotlin/com/sluongng/demo/TodoApplication.kt
+++ src/main/kotlin/com/sluongng/demo/TodoApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication(scanBasePackages = ["com.sluongng.demo"])
-class TodoApplication {
-    fun main(args: Array<String>) {
-        runApplication<TodoApplication>(*args)
-    }
+class TodoApplication
+
+fun main(args: Array<String>) {
+    runApplication<TodoApplication>(*args)
 }
