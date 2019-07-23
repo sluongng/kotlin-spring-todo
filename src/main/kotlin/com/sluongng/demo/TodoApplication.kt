e377a70d0342c75ab25b90e9bef1ac76fcc9418d
Son Luong Ngoc <sluongng@gmail.com>
2019-07-23 09:56:58 +0700

    Added comments and format

--- src/main/kotlin/com/sluongng/demo/TodoApplication.kt
+++ src/main/kotlin/com/sluongng/demo/TodoApplication.kt
@@ -3,9 +3,9 @@ package com.sluongng.demo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class TodoApplication
-
-fun main(args: Array<String>) {
-    runApplication<TodoApplication>(*args)
+@SpringBootApplication(scanBasePackages = ["com.sluongng.demo"])
+class TodoApplication {
+    fun main(args: Array<String>) {
+        runApplication<TodoApplication>(*args)
+    }
 }
